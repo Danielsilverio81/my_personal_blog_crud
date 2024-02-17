3d01b51556fca37ef373fd028d1dbd1085a32da6
danielSdev <danielsilverio81@gmail.com>
2024-02-17 14:33:33 -0300

    add: webpack

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const routes = require('./routes');
 
+app.use(express.urlencoded({extended: true}));
+app.use(routes)
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.send('Ola mundo')
-})
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
 
 app.listen(3000 , () => {
     console.log('Server init in:');
