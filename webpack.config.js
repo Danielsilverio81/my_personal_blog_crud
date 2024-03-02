ca486221f561a60a2eb969057f1ec74b5722c83d
danielSdev <danielsilverio81@gmail.com>
2024-03-02 14:23:13 -0300

    add: css styles

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,24 @@ module.exports = {
                 presets: ['@babel/env']
             }
            } 
-        }, ]
+        },{
+            test:/\.css$/,
+            use: ['style-loader', 'css-loader']
+        } ]
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public'),
+          },
+        compress: true,
+        port: 8081, // Porta para o webpack-dev-server
+        hot: true,
+        proxy: [
+            {
+              context: ['/api'],
+              target: 'http://localhost:3000',
+            },
+          ],
+      },
 };
